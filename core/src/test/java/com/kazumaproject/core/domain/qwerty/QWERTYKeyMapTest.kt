c0a259e8b86ff3f26bdf072b8303d20fa21835f1
yama3nomori <nyama3@yama3nomori.jp>
2026-06-04 10:48:19 +0900

    QWERTY配列でlキーが欠落していたバグを修正

---
+++ core/src/test/java/com/kazumaproject/core/domain/qwerty/QWERTYKeyMapTest.kt
@@ -0,0 +1,41 @@
+package com.kazumaproject.core.domain.qwerty
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class QWERTYKeyMapTest {
+
+    @Test
+    fun testJapaneseQwertyKeyMapping() {
+        val keyMap = QWERTYKeyMap()
+
+        // Verify default Japanese layout mapping (listDefaultJP)
+        val infoLJP = keyMap.getKeyInfoDefaultJP(QWERTYKey.QWERTYKeyL)
+        assertTrue("QWERTYKeyL in listDefaultJP should be a QWERTYVariation", infoLJP is QWERTYKeyInfo.QWERTYVariation)
+        assertEquals('l', (infoLJP as QWERTYKeyInfo.QWERTYVariation).tap)
+
+        val infoMinusJP = keyMap.getKeyInfoDefaultJP(QWERTYKey.QWERTYKeyAtMark)
+        assertTrue("QWERTYKeyAtMark in listDefaultJP should be a QWERTYVariation", infoMinusJP is QWERTYKeyInfo.QWERTYVariation)
+        assertEquals('ー', (infoMinusJP as QWERTYKeyInfo.QWERTYVariation).tap)
+
+        // Verify default Japanese layout with number row mapping (listDefaultJPWithNumberRow)
+        val infoLJPWithNum = keyMap.getKeyInfoDefaultJPWithNumberRow(QWERTYKey.QWERTYKeyL)
+        assertTrue("QWERTYKeyL in listDefaultJPWithNumberRow should be a QWERTYVariation", infoLJPWithNum is QWERTYKeyInfo.QWERTYVariation)
+        assertEquals('l', (infoLJPWithNum as QWERTYKeyInfo.QWERTYVariation).tap)
+
+        val infoMinusJPWithNum = keyMap.getKeyInfoDefaultJPWithNumberRow(QWERTYKey.QWERTYKeyAtMark)
+        assertTrue("QWERTYKeyAtMark in listDefaultJPWithNumberRow should be a QWERTYVariation", infoMinusJPWithNum is QWERTYKeyInfo.QWERTYVariation)
+        assertEquals('ー', (infoMinusJPWithNum as QWERTYKeyInfo.QWERTYVariation).tap)
+    }
+
+    @Test
+    fun testEnglishQwertyKeyMapping() {
+        val keyMap = QWERTYKeyMap()
+
+        // Verify default English layout mapping (listDefault)
+        val infoL = keyMap.getKeyInfoDefault(QWERTYKey.QWERTYKeyL)
+        assertTrue("QWERTYKeyL in listDefault should be a QWERTYVariation", infoL is QWERTYKeyInfo.QWERTYVariation)
+        assertEquals('l', (infoL as QWERTYKeyInfo.QWERTYVariation).tap)
+    }
+}
