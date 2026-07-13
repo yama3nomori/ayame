3f1fa7a90c94aebbc82d5f82c0896f310fb808db
yama3nomori <nyama3@yama3nomori.jp>
2026-07-13 22:41:17 +0900

    「アプリについて」に「田町読みについて」の解説項目を追加、ダイアログ内のURLをクリック可能に修正

--- app/src/main/java/com/kazumaproject/markdownhelperkeyboard/setting_activity/ui/setting/AboutPreferenceFragment.kt
+++ app/src/main/java/com/kazumaproject/markdownhelperkeyboard/setting_activity/ui/setting/AboutPreferenceFragment.kt
@@ -2,7 +2,11 @@ package com.kazumaproject.markdownhelperkeyboard.setting_activity.ui.setting
 
 import android.os.Build
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.text.util.Linkify
 import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -28,6 +32,22 @@ class AboutPreferenceFragment : PreferenceFragmentCompat() {
             } else {
                 "version name: ${packageInfo.versionName}\nversion code: ${packageInfo.versionCode}"
             }
+        }
+
+        val tamachiPreference = findPreference<Preference>("preference_tamachi_reading")
+        tamachiPreference?.setOnPreferenceClickListener {
+            val dialog = AlertDialog.Builder(requireContext())
+                .setTitle(R.string.tamachi_reading_title)
+                .setMessage(R.string.tamachi_reading_dialog_message)
+                .setPositiveButton(android.R.string.ok, null)
+                .create()
+            dialog.show()
+            val messageView = dialog.findViewById<TextView>(android.R.id.message)
+            if (messageView != null) {
+                messageView.movementMethod = LinkMovementMethod.getInstance()
+                Linkify.addLinks(messageView, Linkify.WEB_URLS)
+            }
+            true
         }
 
         val openSourcePreference = findPreference<Preference>("preference_open_source")
